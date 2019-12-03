20e23501df99caa37d28e06eee88772d923e832d
Forrest <11416821+xfox64x@users.noreply.github.com>
2019-12-03 13:30:54 -0500

    Update wigle.py
    Reformatted the plugin to look and work like other plugins. Unsure why the original plugin is completely broke.

--- pwnagotchi/plugins/default/wigle.py
+++ pwnagotchi/plugins/default/wigle.py
@@ -1,3 +1,8 @@
+__author__ = 'forrest'
+__version__ = '1.0.0'
+__license__ = 'GPL3'
+__description__ = 'This plugin automatically uploads collected wifis to wigle.net'
+
 import os
 import logging
 import json
@@ -6,12 +11,100 @@ import csv
 from datetime import datetime
 import requests
 from pwnagotchi.utils import WifiInfo, FieldNotFoundError, extract_from_pcap, StatusFile
-import pwnagotchi.plugins as plugins
 
-'''
-Since documentation is scant, I guess this is a place to say:
-    Copy and paste the "Encoded for use" WiGLE API token into your pwnagotchi config.yaml
-'''
+READY = False
+REPORT = StatusFile('/root/.wigle_uploads', data_format='json')
+SKIP = list()
+OPTIONS = dict()
+
+def on_loaded():
+    global OPTIONS
+    global READY
+    if 'api_key' not in OPTIONS or ('api_key' in OPTIONS and OPTIONS['api_key'] is None):
+        logging.error("WIGLE: api_key isn't set. Can't upload to wigle.net")
+        return
+    READY = True
+
+def on_internet_available(agent):
+    from scapy.all import Scapy_Exception
+    global OPTIONS	
+    global READY
+    global REPORT
+    global SKIP
+
+    # Anti-spaghet
+    if not READY:
+        return
+
+    config = agent.config()
+    display = agent.view()
+    reported = REPORT.data_field_or('reported', default=list())
+    all_gps_files = [os.path.join(config['bettercap']['handshakes'], filename) for filename in os.listdir(config['bettercap']['handshakes']) if filename.endswith('.gps.json')]
+    new_gps_files = set(all_gps_files) - set(reported) - set(SKIP)
+
+    # Anti-spaghet
+    if not new_gps_files:
+        return
+        
+    logging.info("WIGLE: Internet connectivity detected. Uploading new handshakes to wigle.net")
+    no_err_entries = list()
+    data_tuples = []
+    for gps_file in new_gps_files:
+        pcap_filename = gps_file.replace('.gps.json', '.pcap')
+
+        if not os.path.exists(pcap_filename):
+            logging.error("WIGLE: Can't find pcap for %s", gps_file)
+            SKIP.append(gps_file)
+            continue
+
+        try:
+            gps_data = _extract_gps_data(gps_file)
+        except OSError as os_err:
+            logging.error("WIGLE: %s", os_err)
+            SKIP.append(gps_file)
+            continue
+        except json.JSONDecodeError as json_err:
+            logging.error("WIGLE: %s", json_err)
+            SKIP.append(gps_file)
+            continue
+
+        if gps_data['Latitude'] == 0 and gps_data['Longitude'] == 0:
+            logging.warning("WIGLE: Not enough gps-information for %s. Trying again next time.", gps_file)
+            SKIP.append(gps_file)
+            continue
+
+        try:
+            pcap_data = extract_from_pcap(pcap_filename, [WifiInfo.BSSID, WifiInfo.ESSID, WifiInfo.ENCRYPTION, WifiInfo.CHANNEL, WifiInfo.RSSI])
+        except FieldNotFoundError:
+            logging.error("WIGLE: Could not extract all information. Skip %s", gps_file)
+            SKIP.append(gps_file)
+            continue
+        except Scapy_Exception as sc_e:
+            logging.error("WIGLE: %s", sc_e)
+            SKIP.append(gps_file)
+            continue
+
+        data_tuples.append((gps_data, pcap_data))
+        no_err_entries.append(gps_file)
+
+    # Anti-spaghet
+    if len(data_tuples) == 0:
+        return
+
+    display.set('status', "Uploading gps-data to wigle.net ...")
+    display.update(force=True)
+    try:
+        _send_to_wigle(_create_kismet_wigle_csv(data_tuples), OPTIONS['api_key'])
+        reported += no_err_entries
+        REPORT.update(data={'reported': reported})
+        logging.info("WIGLE: Successfully uploaded one file with %s access points.", len(no_err_entries))
+        
+    # The previous exceptions here were as pointless as the one that follows:
+    except Exception as e:
+        # Ignoring valid Wigle data because we hit an exception doesn't seem like a good idea.
+        # We would be throwing away legit, hard-earned data for things like "requests failed because internet sucks"
+        logging.error("WIGLE: Encountered an exception while uploading Kismet Wigle CSV file: %s", str(e))
+
 
 def _extract_gps_data(path):
     """
@@ -68,6 +161,7 @@ def _send_to_wigle(csv_data, api_key, timeout=30):
     """
     Uploads the file to wigle-net
     """
+
     dummy = StringIO()
     dummy.write(csv_data)
     dummy.seek(0)
@@ -82,99 +176,3 @@ def _send_to_wigle(csv_data, api_key, timeout=30):
             raise requests.exceptions.RequestException(json_res['message'])
     except requests.exceptions.RequestException as re_e:
         raise re_e
-
-class Wigle(plugins.Plugin):
-    __author__ = '33197631+dadav@users.noreply.github.com'
-    __version__ = '2.0.0'
-    __license__ = 'GPL3'
-    __description__ = 'This plugin automatically uploads collected wifis to wigle.net'
-
-    def __init__(self):
-        self.ready = False
-        self.report = StatusFile('/root/.wigle_uploads', data_format='json')
-        self.skip = list()
-
-    def on_loaded(self):
-        if 'api_key' not in self.options or ('api_key' in self.options and self.options['api_key'] is None):
-            logging.error("WIGLE: api_key isn't set. Can't upload to wigle.net")
-            return
-        self.ready = True
-
-    def on_internet_available(self, agent):
-        from scapy.all import Scapy_Exception
-        """
-        Called in manual mode when there's internet connectivity
-        """
-
-        # Anti-spaghet
-        if not self.ready:
-            return
-
-        config = agent.config()
-        display = agent.view()
-        reported = self.report.data_field_or('reported', default=list())
-        all_gps_files = [os.path.join(config['bettercap']['handshakes'], filename) for filename in os.listdir(config['bettercap']['handshakes']) if filename.endswith('.gps.json')]
-        new_gps_files = set(all_gps_files) - set(reported) - set(self.skip)
-
-        # Anti-spaghet
-        if not new_gps_files:
-            return
-            
-        logging.info("WIGLE: Internet connectivity detected. Uploading new handshakes to wigle.net")
-        no_err_entries = list()
-        data_tuples = []
-        for gps_file in new_gps_files:
-            pcap_filename = gps_file.replace('.gps.json', '.pcap')
-
-            if not os.path.exists(pcap_filename):
-                logging.error("WIGLE: Can't find pcap for %s", gps_file)
-                self.skip.append(gps_file)
-                continue
-
-            try:
-                gps_data = _extract_gps_data(gps_file)
-            except OSError as os_err:
-                logging.error("WIGLE: %s", os_err)
-                self.skip.append(gps_file)
-                continue
-            except json.JSONDecodeError as json_err:
-                logging.error("WIGLE: %s", json_err)
-                self.skip.append(gps_file)
-                continue
-
-            if gps_data['Latitude'] == 0 and gps_data['Longitude'] == 0:
-                logging.warning("WIGLE: Not enough gps-information for %s. Trying again next time.", gps_file)
-                self.skip.append(gps_file)
-                continue
-
-            try:
-                pcap_data = extract_from_pcap(pcap_filename, [WifiInfo.BSSID, WifiInfo.ESSID, WifiInfo.ENCRYPTION, WifiInfo.CHANNEL, WifiInfo.RSSI])
-            except FieldNotFoundError:
-                logging.error("WIGLE: Could not extract all information. Skip %s", gps_file)
-                self.skip.append(gps_file)
-                continue
-            except Scapy_Exception as sc_e:
-                logging.error("WIGLE: %s", sc_e)
-                self.skip.append(gps_file)
-                continue
-
-            data_tuples.append((gps_data, pcap_data))
-            no_err_entries.append(gps_file)
-
-        # Anti-spaghet
-        if len(data_tuples) == 0:
-            return
-
-        display.set('status', "Uploading gps-data to wigle.net ...")
-        display.update(force=True)
-        try:
-            _send_to_wigle(_create_kismet_wigle_csv(data_tuples), self.options['api_key'])
-            reported += no_err_entries
-            self.report.update(data={'reported': reported})
-            logging.info("WIGLE: Successfully uploaded one file with %s access points.", len(no_err_entries))
-            
-        # The previous exceptions here were as pointless as the one that follows:
-        except Exception as e:
-            # Ignoring valid Wigle data because we hit an exception doesn't seem like a good idea.
-            # We would be throwing away legit, hard-earned data for things like "requests failed because internet sucks"
-            logging.error("WIGLE: Encountered an exception while uploading Kismet Wigle CSV file: %s", str(e))
